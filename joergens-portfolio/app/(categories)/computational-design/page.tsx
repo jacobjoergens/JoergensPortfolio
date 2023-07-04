59aab155baf886c5d63a51c1cc7595226ee49ffa
jacobjoergens <jacob.joergens@gmail.com>
2023-07-04 19:15:11 -0400

    add back button to landing page

--- joergens-portfolio/app/(categories)/computational-design/page.tsx
+++ joergens-portfolio/app/(categories)/computational-design/page.tsx
@@ -1,7 +1,8 @@
 import { notFound } from "next/navigation"
 import { allComputationalProjects } from "contentlayer/generated"
 import Navigation from "@/components/layout/Navigation"
-
+import Link from "next/link"
+import { ArrowUturnLeftIcon } from "@heroicons/react/24/outline"
 import { Metadata } from "next"
 import { Mdx } from "@/components/mdx-components"
 import Image from "next/image"
@@ -14,9 +15,14 @@ interface projectProps {
 
 export default async function projectPage({ params }: projectProps) {
 
-    return (
-      <div>
-        <Navigation listStyle='sidebar-items' linkStyle='sidebar-item' category='computational-design' allItems={allComputationalProjects}/>
-      </div>
-    )
-  }
+  const href = '/'
+  return (
+
+    <div>
+      <Link className={`noSelect backButton`} href={href}>
+        <ArrowUturnLeftIcon className="h-8 w-8" /> Back
+      </Link>
+      <Navigation listStyle='sidebar-items' linkStyle='sidebar-item' category='computational-design' allItems={allComputationalProjects} />
+    </div>
+  )
+}

201e53bf9359f205ca82732fc8553e39a56b29f4
jacobjoergens <jacob.joergens@gmail.com>
2023-08-23 10:04:02 -0400

    Update Layout.tsx

--- joergens-portfolio/components/layout/Layout.tsx
+++ joergens-portfolio/components/layout/Layout.tsx
@@ -2,16 +2,16 @@
 import React, { useState, useEffect } from 'react';
 import Sidebar from './Sidebar';
 import { useWindowSize } from '../../hooks/useWindowSize';
-import { Bars4Icon } from '@heroicons/react/24/outline';
+import styles from 'styles/components/sidebar.module.css'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const windowSize = useWindowSize();
-  const isMobileView = windowSize.width && windowSize.width < 768;
+  // const isMobileView = windowSize.width && windowSize.width < 768;
 
-  useEffect(() => {
-    setIsSidebarOpen(!isMobileView);
-  }, [isMobileView]);
+  // useEffect(() => {
+  //   setIsSidebarOpen(!isMobileView);
+  // }, [isMobileView]);
 
   const handleToggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
@@ -23,17 +23,16 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <div className="layout">
-      <header className={`header ${isMobileView ? '':'with-sidebar'}`}>
-        {isMobileView && (
-          <button className="toggle-button" onClick={handleToggleSidebar} aria-label="Toggle Sidebar">
-            <Bars4Icon className="menuicon noSelect h-6 w-6" />
-          </button>
-        )}
+      {/* <header className="header">
+        <button className="toggle-button" onClick={handleToggleSidebar} aria-label="Toggle Sidebar">
+          <Bars4Icon className="menuicon noSelect h-6 w-6" />
+        </button>
         <p className="title">Jacob Joergens</p>
-      </header>
+      </header> */}
       <div className="content-wrapper">
-        <Sidebar isSidebarOpen={isSidebarOpen} onCloseSidebar={handleCloseSidebar} />
-        <div className={`flex-grow  ${isMobileView && isSidebarOpen ? 'w-full' : ''}`}>{children}</div>
+        <Sidebar isSidebarOpen={isSidebarOpen} handleToggleSidebar={handleToggleSidebar} />
+        <div className={isSidebarOpen ? styles.children + ' ' + styles.open : styles.children}>{children}</div> 
+        {/* className={`flex-grow  ${isSidebarOpen ? 'w-full' : 'w-full'}`} */}
       </div>
     </div>
   );

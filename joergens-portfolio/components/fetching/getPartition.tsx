13ddb5d00e6bd578c76ad842fdc39922cbd6ff25
jacobjoergens <jacob.joergens@gmail.com>
2023-08-23 10:00:13 -0400

    Separate client/server code

---
+++ joergens-portfolio/components/fetching/getPartition.tsx
@@ -0,0 +1,22 @@
+export default async function getPartition(partitionCache: any, degSetIndex: number, index: number) {
+    try {
+        const response = await fetch('/api/getPartition', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                index: index,
+                degSetIndex: degSetIndex
+            })
+        });
+
+        if (response.ok) {
+            const data = await response.text();
+            const resData = JSON.parse(data);
+            partitionCache[degSetIndex][index] = resData.text;
+        } else {
+            console.error('Failed to get partition:', response.status);
+        }
+    } catch (error) {
+        console.error('Error in getPartition:', error);
+    }
+}

1ac301c6887c01ea842965bcd06026ae9f166e28
jacobjoergens <jacob.joergens@gmail.com>
2023-08-25 23:19:59 -0400

    Cut out loadGrasshopper api

---
+++ joergens-portfolio/components/fetching/ghCompute.js
@@ -0,0 +1,31 @@
+import compute from 'compute-rhino3d'
+import path from 'path'
+import fs from 'fs'
+
+export async function runCompute(params) {
+    let data = {}
+
+    const buffer = fs.readFileSync(path.join(process.cwd(), 'ghDefinitions/final.gh'));
+    const definition = new Uint8Array(buffer)
+
+    data.definition = definition
+    data.inputs = params
+
+    // let definitionPath = path.join(process.cwd(), 'app/(categories)/computational-design/sculptural-language/BranchNodeRnd.gh');
+    compute.url = 'http://20.231.1.123:80/'
+    compute.apiKey = '44XyNqF2egQfa7m'
+
+    // set parameters
+    let trees = []
+    if (data.inputs !== undefined) { //TODO: handle no inputs
+        for (let [key, value] of Object.entries(data.inputs)) {
+            let param = new compute.Grasshopper.DataTree(key)
+            param.append([0], Array.isArray(value) ? value : [value])
+            trees.push(param)
+        }
+    }
+    const response = await compute.Grasshopper.evaluateDefinition(definition, trees, false);
+    return await response.json();
+}
+
+

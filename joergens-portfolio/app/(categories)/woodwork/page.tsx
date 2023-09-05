27d75020bdd29e7b8aa454a899ad434c38842fc7
jacobjoergens <jacob.joergens@gmail.com>
2023-09-05 18:42:12 -0400

    Clean up

--- joergens-portfolio/app/(categories)/woodwork/page.tsx
+++ joergens-portfolio/app/(categories)/woodwork/page.tsx
@@ -1,34 +1,7 @@
 'use client'
-import { useEffect, useState } from "react";
-import { useWindowSize } from "@/hooks/useWindowSize";
-import { notFound } from "next/navigation"
 import { allWoodworkingProjects } from "contentlayer/generated"
 import Navigation from "@/components/layout/Navigation"
-import { ArrowUturnLeftIcon, Bars4Icon } from "@heroicons/react/24/outline"
-import Link from "next/link"
 import Layout from "@/components/layout/Layout";
-
-interface projectProps {
-  params: {
-    category: string | undefined
-  }
-}
-
-// async function getProjectsFromParams() {
-//   const projects = allProjects.filter((project) => project.category === 'woodwork')
-
-//   if (!projects) {
-//     null
-//   }
-
-//   return projects
-// }
-
-// export async function generateStaticParams(): Promise<projectProps["params"][]> {
-//   return allProjects.map((project) => ({
-//     category: project.category,
-//   }));
-// }
 
 export default async function projectPage() {
   const href = '/'

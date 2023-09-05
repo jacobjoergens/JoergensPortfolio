015a6b4cfb4e2b706d7fbdade10c968d36c74ae3
jacobjoergens <jacob.joergens@gmail.com>
2023-09-05 18:40:59 -0400

    Add mobile UI

--- joergens-portfolio/app/(categories)/computational-design/protein-earrings/page.tsx
+++ joergens-portfolio/app/(categories)/computational-design/protein-earrings/page.tsx
@@ -4,15 +4,11 @@ import { init } from './initThree.js'
 import styles from "styles/pages/computational.module.css"
 import Spinner from '@/components/layout/Spinner';
 import GUI from '@/components/layout/GUI.js'
-import { ArrowUturnLeftIcon } from '@heroicons/react/24/outline';
+import { ArrowUturnLeftIcon, ArrowRightIcon, ArrowLeftIcon } from '@heroicons/react/24/outline';
 import { Mdx } from '@/components/mdx-components';
 import { allComputationalProjects } from 'contentlayer/generated';
 import Link from 'next/link';
 import PdbSearchBar from '@/components/fetching/pdbSearch';
-// import { downloadPDB } from '@/components/fetching/pdbDownload';
-import path from 'path';
-
-// export let rhino: RhinoModule | null = null;
 
 
 interface ProjectProps {
@@ -55,30 +51,35 @@ async function fetchPdbContent(pdbId: string): Promise<string | null> {
 
 export default function GrasshopperPage({ params }: ProjectProps) {
   const [loading, setLoading] = useState(true);
+  const [openGUI, setOpenGUI] = useState(false);
   const canvasRef = useRef(null);
-  const defaultOption = {label: '1E6V', value: '1E6V', description: 'placeholder'};
+  const defaultOption = { label: '1E6V', value: '1E6V', description: 'placeholder' };
   const [selectedOption, setSelectedOption] = useState<Option | null>(defaultOption);
-  const [atomData, setAtomData] = useState("ATOM      1  N   LEU A   8      40.310  22.627 119.798  1.00 26.69           N"+  
-  "ATOM      2  CA  LEU A   8      40.324  21.151 120.037  1.00 26.66           C"+
-  "ATOM      3  C   LEU A   8      41.637  20.670 120.636  1.00 26.69           C"+
-  "ATOM      4  O   LEU A   8      41.650  19.814 121.533  1.00 26.53           O");
-  
+  const [atomData, setAtomData] = useState("ATOM      1  N   LEU A   8      40.310  22.627 119.798  1.00 26.69           N" +
+    "ATOM      2  CA  LEU A   8      40.324  21.151 120.037  1.00 26.66           C" +
+    "ATOM      3  C   LEU A   8      41.637  20.670 120.636  1.00 26.69           C" +
+    "ATOM      4  O   LEU A   8      41.650  19.814 121.533  1.00 26.53           O");
+
   useEffect(() => {
-    async function download(entryID: string){
+    async function download(entryID: string) {
       const pdbContent = await fetchPdbContent(entryID);
-      if(pdbContent){
+      if (pdbContent) {
         const atom_record = pdbContent
-        .split('\n')
-        .filter(line => line.trim().startsWith('ATOM'))
-        .join('\n'); // Join the filtered lines back into a single string
+          .split('\n')
+          .filter(line => line.trim().startsWith('ATOM'))
+          .join('\n'); // Join the filtered lines back into a single string
         setAtomData(atom_record);
       }
 
     }
-    if(selectedOption){
+    if (selectedOption) {
       download(selectedOption.label);
     }
   }, [selectedOption]);
+
+  const toggleGUI = () => {
+    setOpenGUI(!openGUI);
+  }
 
   const handleSelectChange = (option: Option | null) => {
     setSelectedOption(option);
@@ -94,13 +95,15 @@ export default function GrasshopperPage({ params }: ProjectProps) {
   // }
 
   const currentProjectIndex = allComputationalProjects.findIndex((project) => project.slugAsParams === 'protein-earrings');
+  const nextProject = currentProjectIndex < allComputationalProjects.length - 1 ? allComputationalProjects[currentProjectIndex + 1] : null;
+  const previousProject = currentProjectIndex > 0 ? allComputationalProjects[currentProjectIndex - 1] : null;
 
   const project = allComputationalProjects[currentProjectIndex]
   // const nextProject = currentProjectIndex < allComputationalProjects.length - 1 ? allComputationalProjects[currentProjectIndex + 1] : null;
   // const previousProject = currentProjectIndex > 0 ? allComputationalProjects[currentProjectIndex - 1] : null;
 
-  
-  const href = '/computational-design'
+
+  const href = '/computational-design/'
 
   useEffect(() => {
     setLoading(true);
@@ -129,11 +132,16 @@ export default function GrasshopperPage({ params }: ProjectProps) {
       </div>
       <div className={styles.mainContainer} id='mainContainer'>
         <div className={styles.contentContainer}>
-          <div className={styles.canvasContainer}>
-            {loading && <Spinner />}
-            <canvas className={styles.mainCanvas} id='canvas' ref={canvasRef} />
-            <GUI atomData={atomData} onRenderComplete={handleRenderComplete}/>
-            {/* {`$visibility: loading ? 'visible' : 'hidden' }`} */}
+          <div className={styles.canvasGUI}>
+            <div className={styles.canvasContainer} id='canvas-container'>
+              {loading && <Spinner />}
+              <button className={styles.toggleGUI} onClick={()=>toggleGUI()}>Controls</button>
+              <canvas className={styles.mainCanvas} id='canvas' ref={canvasRef} />
+              {/* {`$visibility: loading ? 'visible' : 'hidden' }`} */}
+            </div>
+            <GUI atomData={atomData} onRenderComplete={handleRenderComplete} openGUI={openGUI} toggle={toggleGUI}/>
+          </div>
+          <div className='searchbar'>
             <PdbSearchBar onChange={handleSelectChange} />
           </div>
         </div>
@@ -141,6 +149,19 @@ export default function GrasshopperPage({ params }: ProjectProps) {
           <Mdx code={project.body.code} />
         </div>
       </div>
+      <div className={styles.pagination}>
+                {previousProject &&
+                    <Link className={`noSelect ${styles.pageButton}`} href={href + previousProject?.slugAsParams}>
+                        <ArrowLeftIcon className='h-8 w-8' /> {previousProject.title}
+                    </Link>
+                }
+                <div className="flex-grow" />
+                {nextProject &&
+                    <Link className={`noSelect ${styles.pageButton} ${styles.nextPage}`} href={href + nextProject?.slugAsParams}>
+                        {nextProject.title} <ArrowRightIcon className='h-8 w-8' />
+                    </Link>
+                }
+            </div>
     </div>
   );
 }
